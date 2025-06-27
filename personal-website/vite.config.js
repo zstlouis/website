659c0a721f2dfe2f81d600c953f6b549fdf95acd
zstlouis <zstlouis7@gmail.com>
2025-06-26 20:57:25 -0400

    edited gh-pages support

--- personal-website/vite.config.js
+++ personal-website/vite.config.js
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
 export default defineConfig({
+  base: "/{your-repo-name}",
   plugins: [react()],
-})
+}) 
